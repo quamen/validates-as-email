d4ae3cce83836a9d6dcc41255cc01abc599e630b
Gareth Townsend <quamen@gmail.com>
2008-07-01 21:23:57 +1000

    initial version of plugin

---
+++ lib/validates_as_email.rb
@@ -0,0 +1,31 @@
+# ValidatesAsEmail
+
+class ValidatesAsEmail
+
+  class EmailAddress
+    require 'resolv'
+    
+    attr_accessor :address
+
+    def initialize(email = '')
+      self.address = email
+    end
+
+    def valid?
+      begin
+        @domain_name = TMail::Address.parse(address).domain
+        return true
+      rescue
+        return false
+      end
+    end
+    
+    def valid_mx_record?
+      Resolv::DNS.open do |dns|
+        @mx = dns.getresources(@domain_name.to_s, Resolv::DNS::Resource::IN::MX)
+      end
+      !@mx.empty?
+    end
+
+  end
+end
